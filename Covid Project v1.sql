dc5f9fdc920b8187ce82de3243f36e38333d60c1
vassomast <125755991+vassomast@users.noreply.github.com>
2023-05-20 21:08:01 +0300

    Title and code updated

--- Covid Project v1.sql
+++ Covid Project v1.sql
@@ -1,111 +1,107 @@
+-- First i check that all the data have been imported
+
 Select * 
-from ProjectPortfolio..CovidDeaths
-order by 3,4
+from CovidProject..CovidDeaths
 
---select * 
---from ProjectPortfolio..CovidVaccinations
---order by 3,4
+select * 
+from CovidProject..CovidVaccinations
 
---select data that we are goinf to be using
+-- From the table CovidDeaths i select the data that i will be using
 
 Select location,date, total_cases, new_cases, total_deaths, population
-from ProjectPortfolio..CovidDeaths
-order by 1,2
+from CovidProject..CovidDeaths
+order by 1,2   --order by location and date to be easier to organise
 
--- looking at total cases vs total deaths
--- shows likelihood of dying if you contract covid
+-- How many deaths have occured versus the total cases per country at the most recent date (for most countries)? (percentage)
+-- Shows likelihood of dying if you contract covid
 
-Select location,date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-from ProjectPortfolio..CovidDeaths
-where location like '%Greece%'
-order by 1,2
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From CovidProject..CovidDeaths
+Where continent is not null and date = '2023-02-10 00:00:00.000'
+order by 1
 
--- looking total cases vs population
--- shows percentage of population that got covid
+-- What is the percentage in Greece from the beginning?
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From CovidProject..CovidDeaths
+Where location like '%Greece%'
+and continent is not null 
+
+
+-- What percentage of population has gotten Covid?
 
 Select location,date, total_cases, population, (total_cases/population)*100 as CasesPercentage
-from ProjectPortfolio..CovidDeaths
+from CovidProject..CovidDeaths
 where location like '%Greece%'
-order by 1,2
 
--- show Countries with highest infection rate compared to Population
+-- Which Countries have the highest infection rate compared to Population?
 
 Select location, population, max(total_cases) as HighestInfectionCount, max((total_cases/population))*100 as CasesPercentage
-from ProjectPortfolio..CovidDeaths
--- where location like '%Greece%'
+from CovidProject..CovidDeaths
 Group by location, population
 order by CasesPercentage desc
 
--- show Countries with highest death count per Population
+-- Which Countries have the highest death numbers
 
-Select location, max(cast(total_deaths as int)) as totaldeathcount
-from ProjectPortfolio..CovidDeaths
--- where location like '%Greece%'
+Select location, max(cast(total_deaths as int)) as totaldeathcount  -- Converted from nvarchar to integer
+from CovidProject..CovidDeaths
 where continent is not null
 Group by location
 order by totaldeathcount desc
 
 -- Break things by continent
 
--- correct way to break by continent
-
-Select location, max(cast(total_deaths as int)) as totaldeathcount
-from ProjectPortfolio..CovidDeaths
--- where location like '%Greece%'
-where continent is null
-Group by location
-order by totaldeathcount desc
-
--- showing continents with highest death count
+-- Which Continents have the highest death numbers?
 
 Select continent, max(cast(total_deaths as int)) as totaldeathcount
-from ProjectPortfolio..CovidDeaths
--- where location like '%Greece%'
+from CovidProject..CovidDeaths
 where continent is not null
 Group by continent
 order by totaldeathcount desc
 
+
 -- GLOBAL NUMBERS
 
 Select date, sum(new_cases), sum (cast(new_deaths as int)) 
-from ProjectPortfolio..CovidDeaths
--- where location like '%Greece%'
+from CovidProject..CovidDeaths
 where continent is not null
 group by date
-order by 1,2
+order by 1
 
--- death percentage
+-- death percentage per day since the beginning
 
 Select date, sum(new_cases) as totalcases, sum (cast(new_deaths as int)) as totaldeaths , sum (cast(new_deaths as int))/sum(new_cases)*100 as deathpercentage
-from ProjectPortfolio..CovidDeaths
--- where location like '%Greece%'
+from CovidProject..CovidDeaths
 where continent is not null
 group by date
-order by 1,2
+order by 1
+
+-- death percentage per total population
 
 Select sum(new_cases) as totalcases, sum (cast(new_deaths as int)) as totaldeaths , sum (cast(new_deaths as int))/sum(new_cases)*100 as deathpercentage
-from ProjectPortfolio..CovidDeaths
--- where location like '%Greece%'
+from CovidProject..CovidDeaths
 where continent is not null
 -- group by date
 order by 1,2
 
 
---looking at total population vs vaccinations
+-- What percentage of population has received at least one vaccine?
 
 Select * 
-from ProjectPortfolio..CovidDeaths dea
-Join ProjectPortfolio..CovidVaccinations vac
+from CovidProject..CovidDeaths dea
+Join CovidProject..CovidVaccinations vac
 on dea.location = vac.location
 and dea.date = vac.date
 
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, sum(convert(int,vac.new_vaccinations)) OVER (Partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
- -- (RollingPeopleVaccinated/population)*100
-from ProjectPortfolio..CovidDeaths dea
-Join ProjectPortfolio..CovidVaccinations vac
-on dea.location = vac.location
-and dea.date = vac.date
-where dea.continent is not null
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From CovidProject..CovidDeaths dea
+Join CovidProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
 order by 2,3
 
 -- Use CTE
